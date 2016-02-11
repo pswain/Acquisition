938d92dbc4630db64c02a7d88b24bf471f777984
Ivan Clark <ivan.clark@ed.ac.uk>
2016-02-11 14:01:03 +0000

    Bug fixes and memory logging.

--- Programmatic GUI/updateFlowData.m
+++ Programmatic GUI/updateFlowData.m
@@ -1,21 +1,19 @@
-function updateFlowData_Callback(hObject, eventdata)
-%
+function updateFlowData(hObject, eventdata, handles)
+%Records information from the gui controls in the flow panel to
+%handles.acquisition.flow
 
-handles=guidata(hObject);
 %If a pump control has been altered, need to know which one it was
-
+handles=guidata(hObject);
 nPumps=2;
 for n=1:nPumps
     volCell=get(handles.(['diameterP' num2str(n)]),'String');
     volString=volCell{get(handles.(['diameterP' num2str(n)]),'Value')};
     handles.acquisition.flow{4}(n).diameter=pump.getDiameter(volString);
     handles.acquisition.flow{4}(n).contents=get(handles.(['contentsP' num2str(n)]),'String');
-    dirCell=get(handles.(['directionP' num2str(n)]),'String');
-    dirString=dirCell{get(handles.(['directionP' num2str(n)]),'Value')};
-    handles.acquisition.flow{4}(n).direction=dirString;
     handles.acquisition.flow{4}(n).currentRate=str2num(get(handles.(['flowRateP' num2str(n)]),'String'));
     handles.acquisition.flow{4}(n).running=get(handles.(['runP' num2str(n)]),'Value');
     handles.acquisition.flow{4}(n).updatePumps;%sends information to the syringe pumps
     handles.acquisition.flow{5}.pumps{n}=handles.acquisition.flow{4}(n);
 end
-guidata(hObject, handles)
+
+guidata(hObject, handles);
--- loadList.m
+++ loadList.m
@@ -7,8 +7,9 @@ for p=1:size(rawdata,1)
     thispoint=rawdata(p,:);
     thispoint=char(thispoint);
     pointScan=textscan(thispoint,'%s %f %f %f %f %f','delimiter',',');
-    name=pointScan{:};
-    
-    points(p,1)=name;
-    points(p,2:6)=pointScan(2:6);
+    if any(~cellfun(@isempty,pointScan))%This will avoid an error from any empty lines being processed
+        name=pointScan{:};
+        points(p,1)=name;
+        points(p,2:6)=pointScan(2:6);
+    end
 end
