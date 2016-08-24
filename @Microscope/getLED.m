54282ab338fdf6397acb6f89fe61563394d4c1e5
Ivan Clark <ivan.clark@ed.ac.uk>
2016-08-24 10:52:01 +0100

    Changes to get loadAcquisition working on Batman.

--- @Microscope/getLED.m
+++ @Microscope/getLED.m
@@ -11,25 +11,29 @@ global mmc;
 switch obj.Name
     case 'Batman'
         if nargin>1%Verbose input from micromanager config for this channel has been supplied
-            %Need to get on Batman to write this bit
-%             digiNames={'Direct Digital Out_P0.0';'Direct Digital Out_P0.1';'Direct Digital Out_P1.0';'Direct Digital Out_P1.1';'Direct Digital Out_P1.2';'Direct Digital Out_P1.3'};
-%             for n=1:length(digiNames)
-%                 usingThisLED=[digiNames{n} '=1'];
-%                 found=strfind(verbose,usingThisLED);
-%                 if ~isempty(found)
-%                     LED=digiNames{n};
-%                 end
-%             end
-        else%No config info supplied - get LED from the state of the switch
+            LEDplace=strfind(verbose,'DTOL-Switch:State=');
+            matVerbose=char(verbose);%Convert to MATLAB string
+            DTolState=matVerbose(LEDplace+18);
+            switch(str2num(DTolState))
+                case 1
+                    LED='DAC0';
+                case 2%The CFP LED - adjust DAC-1
+                    LED='DAC1';
+                case 4%The GFP/YFP LED - adjust DAC-2
+                    LED='DAC2';
+                case 8%The mCherry/cy5/tdTomato LED - adjust DAC-3
+                    LED='DAC3';
+            end
+     else%No config info supplied - get LED from the state of the switch
             dac=mmc.getProperty('DTOL-Switch','State');
             switch(str2num(dac))
                 case 1
                     LED='DAC0';
                 case 2%The CFP LED - adjust DAC-1
                     LED='DAC1';
-                case 4%The GFP/YFP LED - adjust DAC-1
+                case 4%The GFP/YFP LED - adjust DAC-2
                     LED='DAC2';
-                case 8%The mCherry/cy5/tdTomato LED - adjust DAC-1
+                case 8%The mCherry/cy5/tdTomato LED - adjust DAC-3
                     LED='DAC3';
             end
         end
