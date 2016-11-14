3f9b0be0865e9f938c1862caabf7205800b62826
Ivan Clark from mac <ivan.clark@ed.ac.uk>
2016-11-14 11:35:16 +0000

    Added Joker as a demo microscope class (for use when not connected to a microscope)

---
+++ @Joker/Joker.m
@@ -0,0 +1,20 @@
+classdef Joker<Microscope
+    methods
+        function obj=Joker
+            obj.Name='Joker';
+            obj.nameImage=imread('joker.jpg');
+            obj.Config='Volumes/Macintosh HD/Applications/Micro-Manager1.4/MMConfig_demo.cfg';
+            obj.InitialChannel='DIC';
+            obj.Autofocus=Autofocus('');
+            obj.pumpComs(1).com='dummy';%pump1
+            obj.pumpComs(2).com='dummy';%pump2
+            obj.pumpComs(1).baud=19200;
+            obj.pumpComs(2).baud=19200;
+            obj.OmeroInfoPath='~/OmeroDsTemp/';
+            obj.OmeroCodePath='~/Documents/Omero code';
+            obj.DataPath=['/Volumes/Users/' char(java.lang.System.getProperty('user.name')) '/OmeroTemp/'];
+            obj.XYStage='DXYStage';
+            obj.ZStage='DStage';
+        end
+    end
+end
